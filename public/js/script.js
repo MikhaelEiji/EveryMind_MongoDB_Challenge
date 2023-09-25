6aa4c465e988fa24c9038f86cef594d2abfdd3de
Ferreira <g.ferreira@sap.com>
2023-09-24 21:57:32 -0300

    help

--- public/js/script.js
+++ public/js/script.js
@@ -22,16 +22,22 @@ altoContraste.addEventListener("click", () => {
 
 function alteraCorDeFundo(darkMode) {
   const body = document.querySelector('body')
-  const texto = document.querySelector(".texto")
+  const texto = document.querySelectorAll(".texto")
   const texto2 = document.querySelector(".texto2")
 
   if (darkMode === true) {
     body.style.backgroundColor = "#333"
-    texto.style.color = "#fff"
+    texto.forEach(function (elemento) {
+      elemento.style.color = "#fff"
+    }) 
+    // texto.style.color = "#fff"
     texto2.style.color = "#fff"
   } else {
     body.style.backgroundColor = "#f1f1f1"
-    texto.style.color = "#333"
+    texto.forEach(function (elemento) {
+      elemento.style.color = "#333"
+    })
+    // texto.style.color = "#333"
     texto2.style.color = "#333"
   }
 }
